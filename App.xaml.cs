099299f91c308072c7a072bf1219a162bb651abd
kapiosk <29824083+kapiosk@users.noreply.github.com>
2023-11-17 15:59:52 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Simplz.Speedometer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Simplz.Speedometer
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
