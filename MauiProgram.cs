ac2a849c5df361cc5237fa330faa8df0cb56213a
AZ <speedsterk@hotmail.com>
2023-11-19 21:39:49 +0200

    ToggleGeolocation

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,32 +1,26 @@
 ﻿using Microsoft.Extensions.Logging;
-using Simplz.Speedometer.Services;
 
-namespace Simplz.Speedometer
+namespace Simplz.Speedometer;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                });
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
 
-            builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddMauiBlazorWebView();
 
 #if DEBUG
-    		builder.Services.AddBlazorWebViewDeveloperTools();
-    		builder.Logging.AddDebug();
+        builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Logging.AddDebug();
 #endif
 
-#if ANDROID
-        builder.Services.AddSingleton<ILocationService, LocationService>();
-#endif
-
-            return builder.Build();
-        }
+        return builder.Build();
     }
 }
